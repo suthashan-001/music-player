6684e148a139752d04e0f51d4df9885c895a2fe2
Gazer Suthy <suthashan1234@gmail.com>
2022-01-22 16:25:18 -0500

    alignment in being

--- src/mobile/pages/mobile-about.js
+++ src/mobile/pages/mobile-about.js
@@ -1,65 +1,113 @@
 import React from "react";
 
+import chibiLuffy from "../../background-images/chibi-luffy.png";
+import chibiZen from "../../background-images/chibi-zen.png";
+import logo from "../../background-images/gazersuthy_logo.png";
+
 const MobileAbout = () => {
   return (
     <div className="mob-about-bg">
-      <div className="mobile-logo-link-container">
+      <a href="https://www.gazersuthy.com" className="resonate-logo-container">
+        <img
+          src={logo}
+          alt="sun with a diamond inside"
+          className="mobile-logo"
+        />
+
+        <div className="logo-title-container">
+          <h1>
+            Resonate <span className="mob-highlight">432</span>hz
+          </h1>
+          <h5 className="by-gazer">
+            <span className="mob-highlight"> by </span> Gazersuthy
+          </h5>
+        </div>
+      </a>
+
+      <div className="video-link-container">
+        <div>
+          <h3 className="video-link-title">Inspiring Videos</h3>
+          <div className="about-underline"></div>
+        </div>
         <a
-          href="https://www.gazersuthy.com"
-          className="logo-link mobile-logo-link"
+          href="https://www.youtube.com/watch?v=dL1UMZjNlqU"
+          className="mobile-link-container"
         >
-          <div className="resonate-logo">
-            <h1 className="logo-title">
-              Resonate <span className="logo-colour-change">432</span>hz{" "}
-            </h1>
-            <h5>
-              <em>
-                by <span className="logo-colour-change">GazerSuthy</span>
-              </em>
-            </h5>
-          </div>
+          <img src={chibiLuffy} alt="" className="mob-chibiluffy" />
+          <h3>Truth About Music</h3>
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=dL1UMZjNlqU"
+          className="mobile-link-container"
+        >
+          <img src={chibiZen} alt="" className="mob-chibiluffy" />
+          <h3>Why 440hz is Mind Control</h3>
         </a>
       </div>
-      <AboutContainer
-        title={"BLOGS"}
-        color={"lightblue"}
-        links_url={[
-          "https://docs.google.com/document/d/e/2PACX-1vTmckZjqYvdn8IUF2od7FjUaA4ztVfKMeKCIa1yp-ylUgKuTutZbe2VlgPkyGeN5jDYfcgBSc9Z0v67/pub",
-        ]}
-        links_text={["Converting Music to 432hz"]}
-      ></AboutContainer>
-      <AboutContainer
-        title={"VIDEOS"}
-        color={"green"}
-        links_url={[
-          "https://www.youtube.com/watch?v=dL1UMZjNlqU",
-          "https://www.youtube.com/watch?v=rTJVWCUpJEI",
-        ]}
-        links_text={["Truth About Music", "Why 440hz Is Bad"]}
-      ></AboutContainer>
     </div>
   );
 };
 
-const AboutContainer = ({ title, links_url, links_text, color }) => {
-  return (
-    <div className="mob-about-container">
-      <div className="about-head">
-        <h1 className="container-title">{title}</h1>
-        <div className="about-underline"></div>
-      </div>
+// const MobileAbout = () => {
+//   return (
+//     <div className="mob-about-bg">
+//       <div className="mobile-logo-link-container">
+//         <a
+//           href="https://www.gazersuthy.com"
+//           className="logo-link mobile-logo-link"
+//         >
+//           <div className="resonate-logo">
+//             <h1 className="logo-title">
+//               Resonate <span className="logo-colour-change">432</span>hz{" "}
+//             </h1>
+//             <h5>
+//               <em>
+//                 by <span className="logo-colour-change">GazerSuthy</span>
+//               </em>
+//             </h5>
+//           </div>
+//         </a>
+//       </div>
+//       <AboutContainer
+//         title={"BLOGS"}
+//         color={"lightblue"}
+//         links_url={[
+//           "https://docs.google.com/document/d/e/2PACX-1vTmckZjqYvdn8IUF2od7FjUaA4ztVfKMeKCIa1yp-ylUgKuTutZbe2VlgPkyGeN5jDYfcgBSc9Z0v67/pub",
+//         ]}
+//         links_text={["Converting Music to 432hz"]}
+//       ></AboutContainer>
+//       <AboutContainer
+//         title={"VIDEOS"}
+//         color={"green"}
+//         links_url={[
+//           "https://www.youtube.com/watch?v=dL1UMZjNlqU",
+//           "https://www.youtube.com/watch?v=rTJVWCUpJEI",
+//         ]}
+//         links_text={["Truth About Music", "Why 440hz Is Bad"]}
+//       ></AboutContainer>
+//     </div>
+//   );
+// };
 
-      {links_text.map((text, index) => {
-        return (
-          <div className="about-links-container">
-            <a href={`${links_url[index]}`} className={`about-link ${color}`}>
-              {text}
-            </a>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+// const AboutContainer = ({ title, links_url, links_text, color }) => {
+//   return (
+//     <div className="mob-about-container">
+//       <div className="about-head">
+//         <h1 className="container-title">{title}</h1>
+//         <div className="about-underline"></div>
+//       </div>
+
+//       {links_text.map((text, index) => {
+//         return (
+//           <div className="about-links-container">
+//             <a href={`${links_url[index]}`} className={`about-link ${color}`}>
+//               {text}
+//             </a>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
 export default MobileAbout;
--- src/mobile/pages/mobile-sleep.js
+++ src/mobile/pages/mobile-sleep.js
@@ -5,6 +5,11 @@ import gumball from "../../Songs/Sleep/gumball.jpg";
 import stewie from "../../Songs/Sleep/stewie.jpg";
 import bart from "../../Songs/Sleep/bart-sleep.jpg";
 
+import zuko from "../../background-images/zuko.png";
+import katara from "../../background-images/katara.png";
+import toph from "../../background-images/toph.png";
+import aang from "../../background-images/aang.png";
+
 const MobileSleep = () => {
   return (
     <div className="mob-animepg-bg">
@@ -48,6 +53,52 @@ const MobileSleep = () => {
           >
             <img src={bart} alt="" className="card-image sleep-img" />
             <h3>Sleep Music 4</h3>
+          </a>
+        </div>
+      </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div className="mob-page-pannel mob-sleep-pannel">
+        <h1 className="mob-anime-head">Elemental Music</h1>
+        <div className="Dub-card sleep-card mobile-sleep-card">
+          <a
+            className="mobile-music-link"
+            href="https://www.youtube.com/watch?v=szMB0jn4mIo"
+          >
+            <img src={zuko} alt="" className="card-image sleep-img" />
+            <h3>Fire Rhythm</h3>
+          </a>
+        </div>
+
+        <div className="Dub-card sleep-card mobile-sleep-card">
+          <a
+            className="mobile-music-link"
+            href="https://www.youtube.com/watch?v=UiueCXInj3Y"
+          >
+            <img src={katara} alt="" className="card-image sleep-img" />
+            <h3>Water Rhythm</h3>
+          </a>
+        </div>
+        <div className="Dub-card sleep-card mobile-sleep-card">
+          <a
+            className="mobile-music-link"
+            href="https://www.youtube.com/watch?v=u4d7Fvdb654"
+          >
+            <img src={toph} alt="" className="card-image sleep-img" />
+            <h3>Earth Rhythm</h3>
+          </a>
+        </div>
+        <div className="Dub-card sleep-card mobile-sleep-card">
+          <a
+            className="mobile-music-link"
+            href="https://www.youtube.com/watch?v=KgshcWhi7VA"
+          >
+            <img src={aang} alt="" className="card-image sleep-img" />
+            <h3>Air Rhythm</h3>
           </a>
         </div>
       </div>
